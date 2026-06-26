c0f5771746e2682a2ecdbd2d07c6ffb5a533bde5
ben mccarthy <bmccarthy90@gmail.com>
2026-06-26 14:33:46 -0400

    type of food added to columns

--- scrape_test.py
+++ scrape_test.py
@@ -16,34 +16,56 @@ headers = {
 page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.text, "html.parser")
 
-page = pd.read_html(StringIO(str(soup)))
+# not needed
+# page = pd.read_html(StringIO(str(soup)))
 
 # container = soup.find("div", class_="mw-page-container-inner")
 table = soup.find("table", class_="wikitable sortable")
 
 rows = []
+current_title = "regional dishes of the united states"
+# for type_title in soup.find_all("div", class_="mw-heading mw-heading3"):
+    
+#     title = type_title.find_all("h3")
+#     print(title)
 
-for tr in soup.find_all("tr", class_="anchor"):
-    cells = tr.find_all("td")
-    if len(cells) < 5:
-        continue
+# make a type column for each table?
+# tr is table row? td is table data
+for element in soup.find_all(["tr", 'h3']):
+    # print(tr)
+    if element.name == "h3":
+        current_h3 = element.get_text(strip=True)
+        current_title = current_h3
+    
+    elif element.name == "tr" and element.has_attr("class") and "anchor" in element["class"]:
+        cells = element.find_all("td")
+        # skip if 
+        # if len(cells) < 5:
+        #     continue
+        image_tag = cells[0].find("img")
+        image_url = "https:" + image_tag["src"] if image_tag else ""
 
-    image_tag = cells[0].find("img")
-    image_url = "https:" + image_tag["src"] if image_tag else ""
+        dish = cells[1].get_text(strip=True)
+        region = cells[2].get_text(strip=True)
+        location = cells[3].get_text(strip=True)
+        description = cells[4].get_text(strip=True)
+        
+        rows.append({
+            "type" : current_title,
+            "dish": dish,
+            "region": region,
+            "location": location,
+            "description": description,
+            "image_url": image_url
+        })
+    # pprint.pp(rows[0])
+pprint.pp(len(rows))
 
-    dish = cells[1].get_text(strip=True)
-    region = cells[2].get_text(strip=True)
-    location = cells[3].get_text(strip=True)
-    description = cells[4].get_text(strip=True)
-
-    rows.append({
-        "dish": dish,
-        "region": region,
-        "location": location,
-        "description": description,
-        "image_url": image_url
-    })
-pprint.pp(rows)
+# df = pd.DataFrame(rows)
+# pprint.pp(df)
+# df.to_csv("regional_foods_1.csv", index=False)
+# concat description
+# put rows into dataframe
 
 # df = pd.DataFrame(rows)
 
